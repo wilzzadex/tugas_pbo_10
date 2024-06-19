01a1d29a35cc468756ba29aade4d0525f6b23c77
wilzzadex19 <ahmadwaliyudin2018@gmail.com>
2024-06-19 15:21:57 +0700

    update

---
+++ src/pbo_pert_10_ahmadw/Koneksi.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_pert_10_ahmadw;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author waliy
+ */
+public class Koneksi {
+
+    private static java.sql.Connection koneksi;
+
+    public static java.sql.Connection getKoneksi() {
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/tugas_pert_10";
+                String user = "root";
+                String password = "root";
+             
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+            } catch (SQLException t) {
+                System.out.println("Error Membuat Koneksi");
+            }
+        }
+        return koneksi;
+    }
+
+}
